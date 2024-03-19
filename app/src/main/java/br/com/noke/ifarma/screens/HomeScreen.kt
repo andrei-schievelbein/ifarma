3ada862aa6c5aac0d2c7000fc643820ac85a7982
Andrei Schievelvein <andreicel@gmail.com>
2024-03-19 17:26:17 -0300

    Add composable ReceitaEnvio

--- app/src/main/java/br/com/noke/ifarma/screens/HomeScreen.kt
+++ app/src/main/java/br/com/noke/ifarma/screens/HomeScreen.kt
@@ -3,6 +3,11 @@ package br.com.noke.ifarma.screens
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+
+import androidx.compose.foundation.layout.Column
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -11,8 +16,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.FloatingActionButton
@@ -24,33 +32,30 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeScreen() {
-    Enviar()
-    Box(
-        modifier = Modifier
-            .fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(
-            text = "Home",
-            fontFamily = FontFamily.Serif,
-            fontSize = 22.sp
-        )
+
+    Column {
+        ReceitaEnvio()
+        Spacer(modifier = Modifier.height(50.dp))
+        Enviar()
     }
 }
 @Composable
 fun Enviar(modifier: Modifier = Modifier) {
-    FloatingActionButton(
+    Button(
         onClick = { /*TODO*/ },
         modifier = modifier
             .fillMaxWidth()
             .height(55.dp)
             .padding(horizontal = 14.dp),
-            containerColor = Color(0xFF59c606)
+        colors = ButtonDefaults.buttonColors(Color(0xFF59c606))
+//            containerColor = Color(0xFF59c606)
     ) {
         Text(
             text = "Enviar Pedido!",
@@ -61,6 +66,94 @@ fun Enviar(modifier: Modifier = Modifier) {
             )
         )
     }
+}
+
+@Composable
+fun ReceitaEnvio() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(110.dp)
+            .background(Color(0xFFF2F2F2))
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(start = 20.dp, top = 8.dp, end = 20.dp, bottom = 8.dp)
+                .clip(shape = RoundedCornerShape(15.dp))
+                .background(Color.Gray)
+        ) {
+            Row(modifier = Modifier
+                .width(50.dp)
+                .height(110.dp)
+                .background(Color(0xFFFEF7E6))
+                //.border(BorderStroke(1.dp, Color.Magenta))
+            ){
+                Icon(
+                    modifier = Modifier
+                        .padding(
+                            top = 16.dp,
+                            start = 13.dp,
+                            end = 10.dp,
+                            bottom = 10.dp
+                        ),
+                    imageVector = Icons.Filled.DateRange,
+                    contentDescription = "Book"
+                )
+            }
+            Row(modifier = Modifier
+                .width(260.dp)
+                .height(110.dp)
+                .background(Color(0xFFFEF7E6))
+            ){
+                Column (
+                    modifier = Modifier
+                        .padding(
+                            top = 18.dp,
+                            start = 15.dp,
+                            end = 10.dp,
+                            bottom = 10.dp
+                        )
+                ){
+
+                    Text(
+                        text = "Já tem a receita?",
+                        fontFamily = FontFamily.Default,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 14.sp
+                    )
+                    Spacer(modifier =Modifier.height(10.dp))
+
+                    Text(
+                        text = "Envie o seu pedido e receba seus medicamentos!",
+                        fontFamily = FontFamily.Default,
+                        fontSize = 12.sp
+                    )
+                }
+            }
+            Row(modifier = Modifier
+                .fillMaxSize()
+                .height(110.dp)
+                .background(Color(0xFFFEF7E6))
+            ){
+                Icon(modifier = Modifier.padding(
+                    top = 40.dp,
+                    start = 15.dp,
+                    end = 10.dp,
+                    bottom = 10.dp
+                ),
+                    imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
+                    contentDescription = "Seta para a direita"
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun ReceitaEnvioPreview() {
+    ReceitaEnvio()
 }
 
 
